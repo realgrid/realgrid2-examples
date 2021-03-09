3e6cede6fc9362e5eb2e4eaae248303d52b7461e
tacrosst <tacrosst@realgrid.com>
2021-03-09 14:37:45 +0900

    vue.js realgrid2 tree sample 추가

---
+++ realgrid2-vue-tree/src/components/RealGrid.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="real">
+    <div id="realgrid" style="width: 100%; height: 300px"></div>
+  </div>
+</template>
+
+<script>
+import { GridView, LocalDataProvider, TreeView, LocalTreeDataProvider } from 'realgrid';
+import { columns, fields, rows} from './realgrid-data';
+
+let treeView = GridView; 
+let treeDataProvider = LocalDataProvider;
+
+export default {
+  name: 'RealGrid',
+  data: function () {
+      return {
+          
+      }
+  },
+  methods: {
+      loadData: function() {
+      }   
+  },
+  mounted() {
+    treeDataProvider = new LocalTreeDataProvider(true);
+    treeView = new TreeView("realgrid");
+    treeView.setDataSource(treeDataProvider);
+
+    treeDataProvider.setFields(fields);
+    treeView.setColumns(columns);
+  
+    treeDataProvider.setRows(rows, "treeId", false, null, "iconField");
+  }
+}
+</script>
