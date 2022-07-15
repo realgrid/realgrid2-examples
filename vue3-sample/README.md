d5f86afb21afe2774e7cae2b5ba37bc92223fe1a
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2022-07-15 15:23:32 +0900

    vue3 수정

--- vue3-sample/README.md
+++ vue3-sample/README.md
@@ -1,4 +1,4 @@
-# vue3-sample
+# vue3-project
 
 ## Project setup
 ```
--- vue3-sample/src/main.js
+++ vue3-sample/src/main.js
@@ -1,10 +1,6 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app');
 
 require('../node_modules/realgrid/dist/realgrid-style.css');
