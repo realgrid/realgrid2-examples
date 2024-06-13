c77d63f920745e91bf6bab8577f1aa736e7b5b80
onlydel <onlydel@wooritech.com>
2024-06-13 23:19:50 +0900

    라이선스 수정

--- react/nextjs/nextjs-typescript-app-router-realgrid/src/components/RealGrid/index.tsx
+++ react/nextjs/nextjs-typescript-app-router-realgrid/src/components/RealGrid/index.tsx
@@ -2,11 +2,25 @@
 
 import RealGrid from 'realgrid'
 import '../../../node_modules/realgrid/dist/realgrid-style.css'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { RGDataColumn, RealGridReact } from '@realgrid/realgrid-react'
+
+RealGrid.setLicenseKey(
+    'upVcPE+wPOmtLjqyBIh9RkM/nBOseBrflwxYpzGZyYm9cY8amGDkiMnVeQKUHJDjW2y71jtk+wte7L7C4dZzvC/YywSuOuJaA601fFcMg7s='
+  );
 
 const RealGridComponent = () => {
     const grid = useRef(null)
+
+    useEffect(() => {
+        if (grid.current) {
+          const gridView = (grid.current as RealGridReact).gridView;
+          gridView.checkBar.visible = false;
+          gridView.header.height = 36;
+          gridView.displayOptions.rowHeight = 36;
+        }
+      }, [grid]);
+    
     const data = [
         { id: 1, name: 'John Doe', age: 30 },
         { id: 2, name: 'Jane Smith', age: 27 },
@@ -15,7 +29,6 @@ const RealGridComponent = () => {
         { id: 5, name: 'Charlie Davis', age: 28 },
         { id: 6, name: 'Dana White', age: 33 }
     ]
-    RealGrid.setLicenseKey('Your LicenseKey')
 
     return (
         <>
